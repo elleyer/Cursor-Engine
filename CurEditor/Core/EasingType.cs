106a6e00095dccb1c8801f8b0f3ec6004eff487b
Elleyer <shudenkoslavik@gmail.com>
2020-11-23 23:45:06 +0200

    Easings

---
+++ CurEditor/Core/EasingType.cs
@@ -0,0 +1,11 @@
+namespace CurEditor.Core
+{
+    public enum EasingType
+    {
+        Linear = 0,
+        OutCubic = 1,
+        OutElastic = 2,
+        OutExpo = 3,
+        OutQuart = 4
+    }
+}
